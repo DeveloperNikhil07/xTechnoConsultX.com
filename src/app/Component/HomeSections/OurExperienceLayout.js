fce82baf7618da4553b798b299363c484a7bac0e
Code-with-Nikhil-07 <144557756+coder-nikhil-07@users.noreply.github.com>
2025-04-30 23:37:32 +0530

    changes

--- src/app/Component/HomeSections/OurExperienceLayout.js
+++ src/app/Component/HomeSections/OurExperienceLayout.js
@@ -8,26 +8,26 @@ export default function OurExperienceLayout() {
             <section className="skills-counter-wrapper py mt section-bg-mode">
                 <div className="container">
                     <div className="row section-title">
-                        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                            <div className="card border-0 p-2 text-center">
+                        <div className="col-12 mt-sm-3 mt-md-0 col-sm-6 col-md-4 col-lg-3">
+                            <div className="card border py-5 text-center">
                                 <h3><CountUp start={0} end={18} duration={2.75} delay={0.5} separator="," />+</h3>
                                 <p className='pt-2'>Project Completed</p>
                             </div>
                         </div>
-                        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                            <div className="card border-0 p-2 text-center">
+                        <div className="col-12 mt-3 mt-sm-3 mt-md-0 col-sm-6 col-md-4 col-lg-3">
+                            <div className="card border py-5 text-center">
                                 <h3><CountUp start={0} end={2} duration={2.75} delay={0.5} separator="," />+</h3>
                                 <p className='pt-2'>Successful Years</p>
                             </div>
                         </div>
-                        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                            <div className="card border-0 p-2 text-center">
+                        <div className="col-12 mt-3 mt-sm-3 mt-md-0 col-sm-6 col-md-4 col-lg-3">
+                            <div className="card border py-5 text-center">
                                 <h3><CountUp start={0} end={18} duration={2.75} delay={0.5} separator="," />+</h3>
                                 <p className='pt-2'>Client Retention</p>
                             </div>
                         </div>
-                        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-                            <div className="card border-0 p-2 text-center">
+                        <div className="col-12 mt-3 mt-sm-3 mt-lg-0 col-sm-6 col-md-4 col-lg-3">
+                            <div className="card border py-5 text-center">
                                 <h3><CountUp start={0} end={5} duration={2.75} delay={0.5} separator="," />+</h3>
                                 <p className='pt-2'>Countries</p>
                             </div>
