792a1e6e65ccdc114d09f6004ce4c51353fb12a7
Code-with-Nikhil-07 <144557756+coder-nikhil-07@users.noreply.github.com>
2025-05-02 23:21:03 +0530

    new banner video added

---
+++ src/app/Component/HomeSections/PortfolioLayout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function PortfolioLayout() {
+    return (
+        <>
+            <section className='portfolio-wrapper py'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-12'>
+                            <div className='portfolio-content section-title text-center'>
+                                <span className='subtitle'>Portfolio</span>
+                                <h2>Lorem ipsum dolor sit amet consectetur</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Banner from './Component/HomeSections/Banner';
 import WhoWeAre from './Component/HomeSections/WhoWeAre';
 import OurExperienceLayout from './Component/HomeSections/OurExperienceLayout';
+import PortfolioLayout from './Component/HomeSections/PortfolioLayout';
 export default function Home() {
   return (
     <>
       <Banner />
       <WhoWeAre />
       <OurExperienceLayout />
+      <PortfolioLayout />
     </>
   );
 }
