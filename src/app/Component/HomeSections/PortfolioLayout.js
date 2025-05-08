56719f3ce1767ee3bf28e2b2cbd1918de0bdc1c7
Code-with-Nikhil-07 <144557756+coder-nikhil-07@users.noreply.github.com>
2025-05-08 23:34:07 +0530

    changes

--- src/app/Component/HomeSections/PortfolioLayout.js
+++ src/app/Component/HomeSections/PortfolioLayout.js
@@ -12,9 +12,17 @@ export default function PortfolioLayout() {
     const SliderSetting = {
         dots: false,
         infinite: true,
-        speed: 500,
+        speed: 1000,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autplaySpeed:1000,
+        autoplay:true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: { dots: true, arrows: false },
+            },
+        ],
     }
     return (
         <>
@@ -31,14 +39,14 @@ export default function PortfolioLayout() {
                     <div className='row'>
                         <div className='slider-wrapper'>
                             <Slider {...SliderSetting}>
-                                <div data-aos={"slide-up"} data-aos-delay="100" className='slider-card border rounded gap-3 d-flex align-item-center mt-5 align-items-center'>
-                                    <div className='col-12 col-md-6'>
+                                <div data-aos={"slide-up"} data-aos-delay="100" className='slider-card border flex-column flex-lg-row rounded gap-3 d-flex align-item-center mt-5 align-items-center'>
+                                    <div className='col-12 col-lg-6'>
                                         <div className='whoweare-image rounded'>
                                             <Image src={`${GetImages}/assets/images/banner-image.jpg`} alt='Portfolio Image' priority height={500} width={600} />
                                         </div>
                                     </div>
-                                    <div className='col-12 mt-4 mt-md-0 col-md-6'>
-                                        <div className='card-content section-title'>
+                                    <div className='col-12 mt-lg-0 col-lg-6'>
+                                        <div className='p-3 p-lg-0 card-content section-title'>
                                             <p className='pb-4'>Category</p>
                                             <h3>Dave Financial CRM</h3>
                                             <p className='portfolio-dec py-3'>Embark on a journey through our narrative, where innovation meets purpose.</p>
@@ -53,14 +61,14 @@ export default function PortfolioLayout() {
                                     </div>
                                 </div>
                                 {/* Row 2 */}
-                                <div className='slider-card border rounded gap-3 d-flex align-item-center mt-5 align-items-center'>
-                                    <div className='col-12 col-md-6'>
+                                <div data-aos={"slide-up"} data-aos-delay="100" className='slider-card border flex-column flex-lg-row rounded gap-3 d-flex align-item-center mt-5 align-items-center'>
+                                    <div className='col-12 col-lg-6'>
                                         <div className='whoweare-image rounded'>
                                             <Image src={`${GetImages}/assets/images/banner-image.jpg`} alt='Portfolio Image' priority height={500} width={600} />
                                         </div>
                                     </div>
-                                    <div className='col-12 mt-4 mt-md-0 col-md-6'>
-                                        <div className='card-content section-title'>
+                                    <div className='col-12 mt-lg-0 col-lg-6'>
+                                        <div className='p-3 p-lg-0 card-content section-title'>
                                             <p className='pb-4'>Category</p>
                                             <h3>Dave Financial CRM</h3>
                                             <p className='portfolio-dec py-3'>Embark on a journey through our narrative, where innovation meets purpose.</p>
@@ -75,14 +83,14 @@ export default function PortfolioLayout() {
                                     </div>
                                 </div>
                                 {/* Row 3 */}
-                                <div className='slider-card border rounded gap-3 d-flex align-item-center mt-5 align-items-center'>
-                                    <div className='col-12 col-md-6'>
+                                <div data-aos={"slide-up"} data-aos-delay="100" className='slider-card border flex-column flex-lg-row rounded gap-3 d-flex align-item-center mt-5 align-items-center'>
+                                    <div className='col-12 col-lg-6'>
                                         <div className='whoweare-image rounded'>
                                             <Image src={`${GetImages}/assets/images/banner-image.jpg`} alt='Portfolio Image' priority height={500} width={600} />
                                         </div>
                                     </div>
-                                    <div className='col-12 mt-4 mt-md-0 col-md-6'>
-                                        <div className='card-content section-title'>
+                                    <div className='col-12 mt-lg-0 col-lg-6'>
+                                        <div className='p-3 p-lg-0 card-content section-title'>
                                             <p className='pb-4'>Category</p>
                                             <h3>Dave Financial CRM</h3>
                                             <p className='portfolio-dec py-3'>Embark on a journey through our narrative, where innovation meets purpose.</p>
